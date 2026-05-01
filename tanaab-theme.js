a5c945f390c23d798f8f050b6eba17c9664a122b
Mike Pirog <mike@tanaab.dev>
2026-05-01 10:48:30 -0400

    Async load component playground

--- tanaab-theme.js
+++ tanaab-theme.js
@@ -1,13 +1,18 @@
+import { defineAsyncComponent } from 'vue';
+
 import VPLTheme from './tanaab-theme-core.js';
 import TanaabLayout from './layout/TanaabLayout.vue';
 import TMSBox from './components/TMSBox.vue';
-import TMSComponentPlayground from './components/TMSComponentPlayground.vue';
 import TMSGrid from './components/TMSGrid.vue';
 import TMSHero from './components/TMSHero.vue';
 import TMSList from './components/TMSList.vue';
 import TMSLogo from './components/TMSLogo.vue';
 import TMSSection from './components/TMSSection.vue';
 import './styles/theme.scss';
+
+const TMSComponentPlayground = defineAsyncComponent(
+  () => import('./components/TMSComponentPlayground.vue'),
+);
 
 export default {
   ...VPLTheme,
